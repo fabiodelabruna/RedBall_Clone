bc8995ff57f5dc425a68c077a4c346acdec0c630
Fabio Dela Bruna <fabiodelabruna@gmail.com>
2019-06-21 17:27:12 -0300

    Testing some mechanics

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float velocity;
+    public float jumpForce;
+    public float radius;
+    public bool isOnGround;
+    public LayerMask layerMask;
+    public ConstantForce2D constantForce2D;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        isOnGround = Physics2D.OverlapCircle(transform.position, radius, layerMask);
+
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, (rb.velocity.y + 1) * jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            ApplyForce();
+        }
+    }
+
+    void ApplyForce()
+    {
+        float yVelocity = rb.velocity.y;
+        float xInput = Input.GetAxis("Horizontal");
+        float xForce = xInput * velocity * Time.deltaTime;
+
+        if (xInput != 0)
+        {
+            Vector2 force = new Vector2(xForce, 0);
+            rb.AddForce(force, ForceMode2D.Force);
+        }
+
+        if (isOnGround)
+        {
+            if(xInput > 0)
+            {
+                constantForce2D.torque = -1;
+            }
+            else if (xInput < 0)
+            {
+                constantForce2D.torque = 1;
+            }
+            else
+            {
+                constantForce2D.torque = 0;
+            }
+        }
+        else
+        {
+            if(xInput > 0)
+            {
+                constantForce2D.torque = -8;
+            }
+            else if (xInput < 0)
+            {
+                constantForce2D.torque = 8;
+            }
+            else
+            {
+                constantForce2D.torque = 0;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+}
---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public float rotationPeriod;
+
+    private  float lateralLength = 1;
+    private bool isRotating = false;
+    private float xDirection = 0;
+    private float rotationTime = 0;
+    private float radius;
+    private float x;
+    private Vector3 startPosition;
+
+    private Quaternion beforeRotation;
+    private Quaternion afterRotation;
+
+
+    void Start()
+    {
+        x = 1;
+        radius = lateralLength * Mathf.Sqrt(2f) / 2f;
+    }
+
+    void Update()
+    {
+        if (x != 0 && !isRotating)
+        {
+            xDirection = x;
+            startPosition = transform.position;
+            beforeRotation = transform.rotation;
+            transform.Rotate(0, 0, xDirection * 90, Space.World);
+            afterRotation = transform.rotation;
+            transform.rotation = beforeRotation;
+            rotationTime = 0;
+            isRotating = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isRotating)
+        {
+            rotationTime += Time.fixedDeltaTime;
+            float ratioFactor = Mathf.Lerp(0, 1, rotationTime / rotationPeriod);
+
+            float theta = Mathf.Lerp(0, Mathf.PI / 2f, ratioFactor);
+            float xDistance = -xDirection * radius * (Mathf.Cos(45f * Mathf.Deg2Rad) - Mathf.Cos(45f * Mathf.Deg2Rad + theta));
+            float yDistance = radius * (Mathf.Sin(45f * Mathf.Deg2Rad + theta) - Mathf.Sign(45f * Mathf.Deg2Rad));
+            transform.position = new Vector2(startPosition.x + xDistance, startPosition.y + yDistance);
+            transform.rotation = Quaternion.Lerp(beforeRotation, afterRotation, ratioFactor);
+
+            if (ratioFactor == 1) {
+                isRotating = false;
+                xDirection = 0;
+                rotationTime = 0;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("BordersTag"))  
+        {
+            x *= -1;
+        }  
+    }
+
+}
