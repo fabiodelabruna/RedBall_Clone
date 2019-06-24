2cdb89aa340da996c67878b000520b442a81b33b
Fabio Dela Bruna <fabiodelabruna@gmail.com>
2019-06-24 13:51:58 -0300

    Adding some assets and creating GameManager

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D rb;
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("EnemyTag"))
+        {
+            foreach (ContactPoint2D hitPos in other.contacts)
+            {
+                if (hitPos.normal.y > 0 && hitPos.normal.y >= Mathf.Abs(hitPos.normal.x))
+                {
+                    rb.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+                    Destroy(other.gameObject);
+                }
+                else
+                {
+                    rb.AddForce(new Vector2(hitPos.normal.x * 2, hitPos.normal.y * 2), ForceMode2D.Impulse);
+                    if (GameManager.instance.life > 0)
+                    {
+                        GameManager.instance.life--;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public int life;
+
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        life = 3;
+    }
+
+    void Update()
+    {
+        
+    }
+}
